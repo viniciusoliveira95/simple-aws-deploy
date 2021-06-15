2501ec392648fc6af4e2f43de0ba3d4774d4ff9d
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-06-15 14:55:18 -0300

    chore: version 1.1.2

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -3,7 +3,7 @@ describe('APP', () => {
     expect(true).toBe(true)
   })
 
-  test('Simple successful test 2', () => {
+  test('Simple successful test 2 ', () => {
     expect(false).toBe(false)
   })
 })
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 const PORT = 8000
 
-app.get('/', (req, res) => res.send('Simple Express + TypeScript Server is woking on version 1.1.1'))
+app.get('/', (req, res) => res.send('Simple Express + TypeScript Server is woking on version 1.1.2'))
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
