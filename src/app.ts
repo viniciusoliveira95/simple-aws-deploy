4227e8da94b41edcd3537c5ec35948cc461d345b
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-06-16 16:25:32 -0300

    chore: version 2.0.0

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 const PORT = 8000
 
-app.get('/', (req, res) => res.send('Simple Express + TypeScript Server is woking on version 1.1.2'))
+app.get('/', (req, res) => res.send('Simple Express + TypeScript Server is woking on version 2.0.0'))
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
